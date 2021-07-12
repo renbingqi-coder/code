e97fd388e9a59986a9596a305a5c102d0aebfa26
renbingqi <renbingqi@youzan.com>
2021-07-12 09:43:00 +0800

    1

--- JZ1.js
+++ JZ1.js
@@ -13,4 +13,12 @@ function Find(target, array)
 }
 console.log(Find(7,[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]]));
 
-// forin 返回的是属性名
+// for in 返回的是属性名  用来遍历对象除symbol外的属性
+// Object.keys()/Objec.hasOwnProperty()+forin  
+// Object.getOwnPropertyNames()
+// Object.getOwnPropertySymbols()
+// Reflect.ownKeys()
+
+
+
+// for of 直接返回的是值
